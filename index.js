9a39e2441a5fbabd5ec3b2d7c197755cb71e39e6
bobby-brice <bobby.brice@gmail.com>
2020-06-04 22:21:52 +0000

    better save my file with each function working before I refactor

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-const { fetchMyIP } = require('./iss');
+// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
-fetchMyIP((error, ip) => {
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// fetchCoordsByIP("64.137.139.81", (error, data) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+// const coords = {latitude: '43.65550', longitude: '-79.36260'};
+
+// fetchISSFlyOverTimes(coords, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   // let s = new Date(1591307223 * 1000).toLocaleDateString("en-US");
+//   // console.log(s);
+  
+//   console.log("It worked! Returned flyover times: ", passTimes);
+// });
+
+const { nextISSTimesForMyLocation } = require('./iss');
+
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!" , error);
-    return;
+    return console.log("It didn't work!", error);
   }
-
-  console.log('It worked! Returned IP:' , ip);
+  // success, print out the deets!
+  console.log(passTimes);
 });
-
